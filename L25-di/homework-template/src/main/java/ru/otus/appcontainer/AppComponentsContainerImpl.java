0a1aeba8b55b43ea49a2cc94458881a6c939f521
Александр Краснов <krasnov.87@mail.ru>
2020-11-02 23:38:17 +0500

    IoC контейнер

--- L25-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L25-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,8 +1,12 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -16,7 +20,54 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        Method[] methods = configClass.getMethods();
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(method -> method.getAnnotation(AppComponent.class).order()))
+                .forEachOrdered(method -> createComponent(createConstructor(configClass), method));
+    }
+
+    private Object createConstructor(Class<?> configClass) {
+        try {
+            return configClass.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new IllegalArgumentException(String.format("Для класса %S нет конструктора по умолчанию",
+                    configClass.getName()));
+        }
+    }
+
+    private void createComponent(Object configObject, Method method) {
+        Object appComponent = null;
+
+        if (method.getParameterCount() == 0) {
+            appComponent = createObject(configObject, method);
+        } else {
+            Parameter[] parameters = method.getParameters();
+            List<Object> args = new ArrayList<>();
+            for (Parameter parameter : parameters) {
+                var param = getAppComponent(parameter.getType());
+                args.add(param);
+            }
+            appComponent = createObject(configObject, method, args.toArray());
+        }
+
+        appComponents.add(appComponent);
+        appComponentsByName.put(method.getAnnotation(AppComponent.class).name(), appComponent);
+    }
+
+    private Object createObject(Object configObject, Method method, Object... args) {
+        Object appComponents = null;
+        try {
+            if (args == null || args.length == 0) {
+                appComponents = method.invoke(configObject);
+            } else {
+                appComponents = method.invoke(configObject, args);
+            }
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return appComponents;
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -27,11 +78,13 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        return (C) appComponents.stream().filter(object -> componentClass.isAssignableFrom(object.getClass()))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
